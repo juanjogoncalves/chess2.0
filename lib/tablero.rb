fd8df369d4bc0a53a7e1062d7501bc5c5d66762a
juanjogoncalves <juanjogoncalves@gmail.com>
2020-04-25 21:50:43 -0300

    lectura completa - faltan detalles de partida

--- lib/tablero.rb
+++ lib/tablero.rb
@@ -50,8 +50,8 @@ class Tablero
 		self['g1'] = Caballo.new(:blanco)		
 		self['c1'] = Alfil.new(:blanco)
 		self['f1'] = Alfil.new(:blanco)
-    self['d1'] = Dama.new(:blanco)
-    self['e1'] = Rey.new(:blanco)
+    	self['d1'] = Dama.new(:blanco)
+    	self['e1'] = Rey.new(:blanco)
 
 		self['a8'] = Torre.new(:negro)
 		self['h8'] = Torre.new(:negro)
@@ -59,8 +59,8 @@ class Tablero
 		self['g8'] = Caballo.new(:negro)		
 		self['c8'] = Alfil.new(:negro)
 		self['f8'] = Alfil.new(:negro)		
-    self['d8'] = Dama.new(:negro)
-    self['e8'] = Rey.new(:negro)
+    	self['d8'] = Dama.new(:negro)
+    	self['e8'] = Rey.new(:negro)
 	end
 
 	def dibujar
@@ -159,36 +159,19 @@ class Tablero
 		adjacentes
 	end
 
-	
-=begin def jaque(jugada,color)
-		resultado = []
-
-		('a'..'h').each do |fil|
-			(1..8).each do |col|
-				unless self["#{fil}#{col}"].vacia?
-					resultado << "#{self["#{fil}#{col}"].posicion}" if self["#{fil}#{col}"].movimientos_permitidos.include?(jugada) && self["#{fil}#{col}"].color != color
-				end
-			end
-		end
-		puts resultado
-		resultado == []
-	end
-=end
-	
-
 	def pieza_origen(jugada, color)
 		pos_pieza = []
 		resultado = ''
+		rsp = ''
 	
-		if jugada[-1] == '+' && jugada[-2] == '+'
-			xy = "#{jugada[-4]}#{jugada[-3]}"
-		elsif jugada[-1] == '+'
-			xy = "#{jugada[-3]}#{jugada[-2]}"
-		else					
-			xy = "#{jugada[-2]}#{jugada[-1]}"
+		jugada.split('').each do |e|
+			unless e == 'x' || e == '+'
+				rsp << e
+			end
 		end
-	
-		
+		 
+		xy = "#{rsp[-2]}#{rsp[-1]}"	
+
 		('a'..'h').each do |col|
 			(1..8).each do |fila|
 					unless self["#{col}#{fila}"].vacia?
@@ -199,40 +182,30 @@ class Tablero
 
 		pos_pieza.each do |pieza| 
 	
-			if @piezas[pieza].class == Peon && jugada[0] == pieza[0] && @piezas[pieza].color == color
+			if @piezas[pieza].class == Peon && rsp[0] == pieza[0] && @piezas[pieza].color == color
 				resultado << @piezas[pieza].posicion
 			end
 
-			if jugada.size == 4 && jugada[1] != 'x'
-				if @piezas[pieza].notacion? == jugada[0] && @piezas[pieza].color == color && jugada[1] == pieza[0]
+			if rsp.size == 4 
+				if @piezas[pieza].notacion? == rsp[0] && @piezas[pieza].color == color && rsp[1] == pieza[0]
 					resultado << @piezas[pieza].posicion
 				end
-			else
-				if @piezas[pieza].notacion? == jugada[0] && @piezas[pieza].color == color
+			end
+			
+			if rsp.size == 3
+				if @piezas[pieza].notacion? == rsp[0] && @piezas[pieza].color == color
 					resultado << @piezas[pieza].posicion
 				end	
-			end	
-			
+			end
 		end
 
 		resultado
-  
-  end
+    end
 
   
   def movimiento(t, jugada, color)
 		
-  		if jugada[-1] == '+' && jugada[-2] == '+'
-						xy = "#{jugada[-4]}#{jugada[-3]}"
-			
-			elsif jugada[-1] == '+'
-						xy = "#{jugada[-3]}#{jugada[-2]}"
-			
-			else					
-						xy = "#{jugada[-2]}#{jugada[-1]}"
-			end
-
-			if jugada == 'O-O'
+		if jugada == 'O-O'
   			if color == :negro
   				self['e8']= PiezaVacia.new
   				self['h8']= PiezaVacia.new
@@ -240,6 +213,7 @@ class Tablero
   				self['f8']= Torre.new(:negro)
   				dibujar
   			end
+  		
   			if color == :blanco
   				self['e1']= PiezaVacia.new
   				self['h1']= PiezaVacia.new
@@ -263,12 +237,23 @@ class Tablero
   				self['d1']= Torre.new(:blanco)
   				dibujar
   			end
- 			else
-			
-					p_origen= @piezas[pieza_origen(jugada,color)]
-					@piezas["#{xy}"] = p_origen.clone
-					self[p_origen.posicion] = PiezaVacia.new
-					dibujar
+ 		else
+ 			rsp = ''
+
+ 			jugada.split('').each do |e|
+				unless e == 'x' || e == '+'
+					rsp << e
+				end
 			end
+		 
+			xy = "#{rsp[-2]}#{rsp[-1]}"	
+
+			p_origen= @piezas[pieza_origen( jugada, color)]
+			@piezas["#{xy}"] = p_origen.clone
+			self[p_origen.posicion] = PiezaVacia.new
+			dibujar
+		end
+
+		
 	end
 end
